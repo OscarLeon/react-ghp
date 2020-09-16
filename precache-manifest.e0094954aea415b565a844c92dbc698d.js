9bd473ff19638a1b1931d4dbc272800f6a30ea95
Oscar Matias León Trureo <oleontru@MacBook-Air-de-Oscar.local>
2020-09-15 22:03:09 -0300

    Updates

---
+++ precache-manifest.e0094954aea415b565a844c92dbc698d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b644d0307aee4ef39389cb41ce2ae0",
+    "url": "/react-ghp/index.html"
+  },
+  {
+    "revision": "69c8fb7655e509946f88",
+    "url": "/react-ghp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0fcee65e3fe28bff4add",
+    "url": "/react-ghp/static/js/2.95170572.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-ghp/static/js/2.95170572.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69c8fb7655e509946f88",
+    "url": "/react-ghp/static/js/main.23d18f0e.chunk.js"
+  },
+  {
+    "revision": "c0d157d9d1d65b57625e",
+    "url": "/react-ghp/static/js/runtime-main.de198505.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-ghp/static/media/logo.5d5d9eef.svg"
+  }
+]);
